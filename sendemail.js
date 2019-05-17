6cdc5ccc1a1275409f9f1bfa190d0e853e28ce9f
realzam <sza0210escom@gmail.com>
2019-05-16 22:30:21 -0500

    Create sendemail.js

---
+++ sendemail.js
@@ -0,0 +1,25 @@
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'enigmalibro@gmail.com',
+    pass: 'adoescom2019'
+  }
+});
+
+var mailOptions = {
+  from: 'enigmalibro@gmail.com',
+  to: 'sza0210escom@gmail.com, emmayuel80@gmail.com',
+  subject: 'Sending Email using Node.js',
+  text: `The game`
+  // html: '<h1>Hi Smartherd</h1><p>Your Messsage</p>'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
